b6ac610cb49d1d7d49b95b8574f68d089117a00b
FrozenJellyfish <144744024+FrozenJellyfish@users.noreply.github.com>
2023-09-12 21:42:35 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,142 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["848314f8-d7dc-4755-a4a1-3782173096b1","26b80e63-bc0f-408d-b288-be2282aebd4e","dfc53be7-786c-4305-b8eb-81fa595e9903","47fade86-5bd1-4789-87af-896fb1433a7b","043deebf-64b8-4795-be8d-db5055414f2f","c26b6449-0b6c-4483-8486-107c0091c26e","3221caad-ae37-4eec-92a8-56bae4769e66","c4e310e0-8174-4127-a46e-0cbcc94b488b","0f9412ac-0872-4a20-9970-cb77e7b408c0","bcf75a6e-4809-41c3-aeeb-768b47add7d7","1b81b763-949a-4af0-b07a-92b84f7a9b2e"],"propsByKey":{"848314f8-d7dc-4755-a4a1-3782173096b1":{"name":"hero","sourceUrl":null,"frameSize":{"x":30,"y":30},"frameCount":1,"looping":true,"frameDelay":12,"version":"eqBiMWLs_l_rIDRoPwKNbmwaXc92vKbH","categories":["sports"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":30,"y":30},"rootRelativePath":"assets/848314f8-d7dc-4755-a4a1-3782173096b1.png"},"26b80e63-bc0f-408d-b288-be2282aebd4e":{"name":"enemy1","sourceUrl":null,"frameSize":{"x":35,"y":50},"frameCount":1,"looping":true,"frameDelay":12,"version":"apkicFwBxRGw7Z4V_kq_Wexn1XWXQfAy","categories":["icons"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":35,"y":50},"rootRelativePath":"assets/26b80e63-bc0f-408d-b288-be2282aebd4e.png"},"dfc53be7-786c-4305-b8eb-81fa595e9903":{"name":"enemy","sourceUrl":"assets/api/v1/animation-library/gamelab/xasculQGiYxBV79ltD_0E79ZRIexdPdZ/category_food/american_hamburger.png","frameSize":{"x":320,"y":254},"frameCount":1,"looping":true,"frameDelay":2,"version":"xasculQGiYxBV79ltD_0E79ZRIexdPdZ","categories":["food"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":320,"y":254},"rootRelativePath":"assets/api/v1/animation-library/gamelab/xasculQGiYxBV79ltD_0E79ZRIexdPdZ/category_food/american_hamburger.png"},"47fade86-5bd1-4789-87af-896fb1433a7b":{"name":"enemy2","sourceUrl":"assets/api/v1/animation-library/gamelab/dVaFR7XFVlGQX4d.e71iiKWvnLhMbaxG/category_food/american_pastrami.png","frameSize":{"x":355,"y":241},"frameCount":1,"looping":true,"frameDelay":2,"version":"dVaFR7XFVlGQX4d.e71iiKWvnLhMbaxG","categories":["food"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":355,"y":241},"rootRelativePath":"assets/api/v1/animation-library/gamelab/dVaFR7XFVlGQX4d.e71iiKWvnLhMbaxG/category_food/american_pastrami.png"},"043deebf-64b8-4795-be8d-db5055414f2f":{"name":"enemy3","sourceUrl":"assets/api/v1/animation-library/gamelab/YSis4_lex43su6FLaL__bhoag4eHAl8D/category_food/american_bbqribs.png","frameSize":{"x":388,"y":388},"frameCount":1,"looping":true,"frameDelay":2,"version":"YSis4_lex43su6FLaL__bhoag4eHAl8D","categories":["food"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":388,"y":388},"rootRelativePath":"assets/api/v1/animation-library/gamelab/YSis4_lex43su6FLaL__bhoag4eHAl8D/category_food/american_bbqribs.png"},"c26b6449-0b6c-4483-8486-107c0091c26e":{"name":"hero1","sourceUrl":"assets/api/v1/animation-library/gamelab/loycQXdICpzI4PfXITdIndG9GcVBmRdK/category_faces/kidportrait_01.png","frameSize":{"x":264,"y":368},"frameCount":1,"looping":true,"frameDelay":2,"version":"loycQXdICpzI4PfXITdIndG9GcVBmRdK","categories":["faces"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":264,"y":368},"rootRelativePath":"assets/api/v1/animation-library/gamelab/loycQXdICpzI4PfXITdIndG9GcVBmRdK/category_faces/kidportrait_01.png"},"3221caad-ae37-4eec-92a8-56bae4769e66":{"name":"b","sourceUrl":"assets/api/v1/animation-library/gamelab/IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT/category_backgrounds/kitchen.png","frameSize":{"x":400,"y":400},"frameCount":1,"looping":true,"frameDelay":2,"version":"IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT","categories":["backgrounds"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":400,"y":400},"rootRelativePath":"assets/api/v1/animation-library/gamelab/IJemJVUh3J1gcGlCdIJ8obCWhXAqxbUT/category_backgrounds/kitchen.png"},"c4e310e0-8174-4127-a46e-0cbcc94b488b":{"name":"dream","sourceUrl":null,"frameSize":{"x":386,"y":268},"frameCount":1,"looping":true,"frameDelay":12,"version":"zgCMMM7aIlQYvm2N8NGidoJBdYm9stXL","categories":["icons"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":386,"y":268},"rootRelativePath":"assets/c4e310e0-8174-4127-a46e-0cbcc94b488b.png"},"0f9412ac-0872-4a20-9970-cb77e7b408c0":{"name":"nave","sourceUrl":null,"frameSize":{"x":167,"y":119},"frameCount":1,"looping":true,"frameDelay":12,"version":"nDptEcH90AZVtRRV34LXm5vXCiejpnFY","categories":["retro"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":167,"y":119},"rootRelativePath":"assets/0f9412ac-0872-4a20-9970-cb77e7b408c0.png"},"bcf75a6e-4809-41c3-aeeb-768b47add7d7":{"name":"inimigos","sourceUrl":null,"frameSize":{"x":392,"y":392},"frameCount":1,"looping":true,"frameDelay":12,"version":"FTtlyKqHgJgggmRykNpA7YFaN7B5Qs70","categories":["retro"],"loadedFromSource":true,"saved":true,"sourceSize":{"x":392,"y":392},"rootRelativePath":"assets/bcf75a6e-4809-41c3-aeeb-768b47add7d7.png"},"1b81b763-949a-4af0-b07a-92b84f7a9b2e":{"name":"space","sourceUrl":null,"frameSize":{"x":1000,"y":563},"frameCount":1,"looping":true,"frameDelay":12,"version":"zHYaN4cpWwycDKBjoryuL7V9t99UGy73","categories":[""],"loadedFromSource":true,"saved":true,"sourceSize":{"x":1000,"y":563},"rootRelativePath":"assets/1b81b763-949a-4af0-b07a-92b84f7a9b2e.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+
+ 
+var hero = createSprite(200,365,20,20)
+hero.shapeColor="red"
+hero.setAnimation('nave');
+hero.scale=0.3
+
+var enemy1 = createSprite(200,290,10,10)
+enemy1.shapeColor="red"
+enemy1.setAnimation('inimigos')
+enemy1.scale=0.1
+
+var enemy2 = createSprite(200,170,10,10)
+enemy2.shapeColor="red"
+enemy2.setAnimation('inimigos')
+enemy2.scale=0.1
+
+var enemy3 = createSprite(200,50,10,10)
+enemy3.shapeColor="red"
+enemy3.setAnimation('inimigos')
+enemy3.scale=0.1
+
+var net = createSprite(200,5,100,10)
+net.shapeColor="red"
+
+var goal =0;
+var death = 0
+
+
+
+enemy1.setVelocity(-10,0);
+enemy2.setVelocity(10,0);
+enemy3.setVelocity(-10,0);
+
+
+function draw() {
+  
+//plano de fundo(b);
+background('white')
+createEdgeSprites()
+drawSprites()
+
+
+
+enemy1.bounceOff(edges)
+enemy2.bounceOff(edges)
+enemy3.bounceOff(edges)
+
+if(keyDown(UP_ARROW)){
+  hero.y=hero.y-4
+}
+
+if(keyDown(DOWN_ARROW)){
+  hero.y=hero.y+4
+}
+
+if(keyDown(LEFT_ARROW)){
+  hero.x=hero.x-4
+}
+
+if(keyDown(RIGHT_ARROW)){
+  hero.x=hero.x+4
+}
+
+if(hero.isTouching(enemy1)|| hero.isTouching(enemy2)|| hero.isTouching(enemy3)){
+  playSound("assets/category_achievements/bubbly_game_achievement_sound.mp3")
+  hero.x=200
+  hero.y=350
+  death = death+1
+}
+if(hero.isTouching(net)){
+  playSound("assets/category_achievements/vibrant_game_game_gold_tresure_chest_open.mp3")
+  hero.x=200
+  hero.y=345
+  goal=goal+1
+}
+textSize(20)
+  fill("black")
+  text("vitórias:"+goal,20,380);
+  
+
+textSize(20)
+  fill("black")
+  text("mortes:"+death,20,350);
+  
+
+}
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
